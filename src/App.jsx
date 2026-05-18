9206ac20bf800741fa8a7d3332db5b292eeebab9
MaximoPate <patem530@gmail.com>
2026-05-18 11:29:01 -0300

    viewLanding creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { supabase } from './utils/supabase'
+import ViewLanding from './viewLanding/ViewLanding'
 
 function App() {
-  const [productos, setProductos] = useState([])
-
-
-  supabase.from("Producto").select("nombre").then(res =>{
-    console.log(res.data);
-     
-  })
-
-
-  useEffect(() => {
-    async function getProductos() {
-      const { data, error } = await supabase
-        .from('Producto')
-        .select('*')
-        
-      if (data) {
-        setProductos(data)
-      }
-
-      if (error) {
-        console.log(error)
-      }
-    }
-
-    getProductos()
-  }, [])
-
-  
   return (
-    <ul>
-      {productos.map((producto) => (
-        <li key={producto.id_producto}>
-          {producto.nombre} - ${producto.precio}
-        </li>
-      ))}
-    </ul>
+    <ViewLanding/>
   )
 }
 
---
+++ src/viewLanding/ViewLanding.jsx
@@ -0,0 +1,18 @@
+function viewLanding(){
+
+    return (
+    <div className="view-local">
+      <CarruselFachadaLocal />
+      <FiltroLocal />
+      <main className="view-content">
+        <SeccionPrendas titulo="Productos Destacados" productos={productosDestacados} tipo="destacados" />
+        <SeccionPrendas titulo="Productos SALE 🔥" productos={productosSale} tipo="sale" />
+        <div className="spacer-footer" />
+      </main>
+      <Footer />
+    </div>
+    )
+
+}
+
+export default viewLocal
