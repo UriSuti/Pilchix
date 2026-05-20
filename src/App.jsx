91a6b2c709518bebb530c00def606da8306f1c88
ALAN GABRIEL GORDON <48860730@ort>
2026-05-20 08:42:55 -0300

    avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,14 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { supabase } from './utils/supabase'
-import ViewLanding from './viewLanding/ViewLanding'
+import ViewLocal from './viewLocal/viewLocal'
 
 function App() {
-  const [productos, setProductos] = useState([])
 
-
-  supabase.from("Producto").select("nombre").then(res =>{
-    console.log(res.data);
-     
-  })
-
-
-  useEffect(() => {
-    async function getProductos() {
-      const { data, error } = await supabase
-        .from('Producto')
-        .select('*')
-        
-      if (data) {
-        setProductos(data)
-      }
-
-      if (error) {
-        console.log(error)
-      }
-    }
-
-    getProductos()
-  }, [])
 
   
   return (
-    <ul>
-      {productos.map((producto) => (
-        <li key={producto.id_producto}>
-          {producto.nombre} - ${producto.precio}
-        </li>
-      ))}
-    </ul>
+    <ViewLocal/>
   )
 }
 
