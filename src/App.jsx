e677331921f640b2b2c35319a9a8fd87233003b0
URIEL SUTI <49194950@ort>
2026-05-29 09:04:27 -0300

    Un poque de tode

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import { supabase } from './utils/supabase'
 import ViewLocal from './viewLocal/ViewLocal'
 import ViewLanding from './viewLanding/ViewLanding'
+import { getLandingCategoriasConProductos } from './viewLanding/services/landing'
 
 function App() {
 
   const [local, setLocal] = useState(null);
 
+  console.log(getLandingCategoriasConProductos())
+
 
   return (
     local !== null? <ViewLocal local={local} /> : <ViewLanding local={local} setLocal={setLocal} />
-    
   )
 }
 
