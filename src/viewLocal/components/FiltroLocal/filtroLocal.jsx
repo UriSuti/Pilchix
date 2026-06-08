d242e200eb2b116f90aca8d69c51e2e5f043836e
URIEL SUTI <49194950@ort>
2026-06-08 08:41:58 -0300

    mas css

--- src/viewLocal/components/FiltroLocal/filtroLocal.jsx
+++ src/viewLocal/components/FiltroLocal/filtroLocal.jsx
@@ -1,17 +1,22 @@
-import { useEffect } from 'react';
 import './filtroLocal.css'
 
-function FiltroLocal({ search, setSearch, orden, setOrden }){
-
+function FiltroLocal({ search, setSearch, orden, setOrden }) {
   return (
     <section className="filtro-local">
       <div className="contenedor-filtros">
-        {/* Buscador */}
         <div className="busqueda-productos">
-          <input type="text" placeholder="Buscar productos" value={search} onChange={(e) => setSearch(e.target.value)}/>
+          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <circle cx="11" cy="11" r="8" />
+            <line x1="21" y1="21" x2="16.65" y2="16.65" />
+          </svg>
+          <input
+            type="text"
+            placeholder="Buscar productos"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
 
-        {/* Select de orden */}
         <div className="ordenar-productos">
           <select value={orden} onChange={(e) => setOrden(e.target.value)}>
             <option value="">Ordenar</option>
@@ -21,7 +26,6 @@ function FiltroLocal({ search, setSearch, orden, setOrden }){
           </select>
         </div>
 
-        {/* Texto lateral */}
         <div className="suscripcion">
           <p>
             recibir notificaciones de novedades{" "}
@@ -31,9 +35,6 @@ function FiltroLocal({ search, setSearch, orden, setOrden }){
       </div>
     </section>
   );
-
-
-
-
 }
-export default FiltroLocal
+
+export default FiltroLocal;
