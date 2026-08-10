37373d2686851ef9834a5f9deec159c56adc7846
URIEL SUTI <49194950@ort>
2026-08-10 11:33:29 -0300

    se viene mobile

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 const TOKEN_KEY = "pilchix_token";
 const TOKEN_MARCA_KEY = "pilchix_token_marca";
